29d9fa5c5a4b4090fe8ded08a71f9ac3f1c4306e
Nathan Pire <nathan.pire@gmail.com>
2019-07-28 14:12:52 +0200

    creating models

---
+++ API/EventOrganizer/EventOrganizer.Model/Ong.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventOrganizer.Model
+{
+    public class Ong:Entity
+    {
+        public string Name { get; set; }
+        public string Website { get; set; }
+        public List<TimeTableOng> TimeTables { get; set; }
+    }
+}
