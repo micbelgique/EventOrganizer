a0613e000bfa8d9913d0372802f9062d421bcc2f
thelittlewozniak <nathan.pire@gmail.com>
2019-08-22 21:10:06 +0200

    generate new project for package

---
+++ Angular/eventOrganizerLibrary/projects/library/src/lib/library.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-library',
+  template: `
+    <p>
+      library works!
+    </p>
+  `,
+  styles: []
+})
+export class LibraryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ Angular/eventOrganizerLibrary/projects/library/src/lib/library.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { LibraryComponent } from './library.component';
+
+@NgModule({
+  declarations: [LibraryComponent],
+  imports: [
+  ],
+  exports: [LibraryComponent]
+})
+export class LibraryModule { }
